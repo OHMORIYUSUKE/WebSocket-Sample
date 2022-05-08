8bf807bb552a54abd251b3e68599474561bda294
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-05-08 21:45:23 +0900

    修正

--- backend/app/static/script/app.js
+++ backend/app/static/script/app.js
@@ -1,4 +1,4 @@
-var ws = new WebSocket("ws://localhost:8000/ws");
+var ws = new WebSocket("ws://localhost/ws");
 
 const stalkerMe = document.getElementById('stalkerMe'); 
 const stalkerEnemy = document.getElementById('stalkerEnemy'); 
